d550a842c18136d16c9d3cca3f9fb851fafb1e1d
Ryan Kray <rrich.kray.93@gmail.com>
2023-04-03 10:32:41 -0400

    worked on loading page

--- src/components/LoadingCircle/LoadingCircle.jsx
+++ src/components/LoadingCircle/LoadingCircle.jsx
@@ -6,7 +6,10 @@ const LoadingCircle = () => {
         <div className={styles.loadingScreen}>
             <div className={styles.contentContainer}>
                 <div className={styles.square}>
-                    <div className={styles.particle}></div>     
+                    <div className={styles.particle} style={{top: 0, right: 0}}></div>  
+                    <div className={styles.particle} style={{top: 0, left: 0}}></div>  
+                    <div className={styles.particle} style={{bottom: 0, right: 0}}></div>  
+                    <div className={styles.particle} style={{bottom: 0, left: 0}}></div>  
                 </div>
                 <div className={styles.loadingText}>
                     <p>Loading</p>
